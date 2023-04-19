3888c2e575d9368652301ce1d73bb4a54e352798
tinochan <tinokwchan@gmail.com>
2023-04-19 19:13:29 +0800

    Completed the todos

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -4,6 +4,8 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 /**
  * Learn how to transform values.
  *
@@ -11,25 +13,31 @@ import reactor.core.publisher.Mono;
  */
 public class Part04Transform {
 
+	Function<User, User> capitalize = user -> new User(
+		user.getUsername().toUpperCase(),
+		user.getFirstname().toUpperCase(),
+		user.getLastname().toUpperCase()
+	);
+
 //========================================================================================
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(capitalize);
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(capitalize);
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
